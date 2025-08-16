c60e8fbd49e00edfb2a9f0da83c34b63a4cc97e2
sam <141178315+SamarthMore31@users.noreply.github.com>
2025-08-16 20:04:05 +0530

    Delete utils directory

--- utils/pdf_reader.py
+++
@@ -1,8 +0,0 @@
-from pdfminer.high_level import extract_text
-
-def extract_text_from_pdf(file_path):
-    try:
-        return extract_text(file_path)
-    except Exception as e:
-        print(f"Error extracting PDF text: {e}")
-        return ""
